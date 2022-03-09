41bfb608215cf90bc5a0b6bbf4c123a410a7d120
vishuprem99 <vishwashibare29@gmail.com>
2022-03-09 15:15:58 +0530

    project added

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flora/drawer/navdrawer.dart';
+import 'package:flutter/material.dart';
+import 'bottom_button/bottom_slider.dart';
+import 'drawer/navdrawer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        appBar: AppBar(
+         
+        ),
+        
+        drawer: navdrawer(),
+        body: Column(
+          children: [
+            
+           MyHomePage(),
+           
+          
+           
+                 
+                 
+               
+               
+            
+          ],
+       
+        
+        
+      ),
+        ),
+      );
+
+
+      
+    
+  }
+}
